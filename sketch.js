2dc4b34cce6d2b8449265fa17acf2d59ebbb212b
krrishsetia <88411219+krrishsetia@users.noreply.github.com>
2021-09-30 00:31:13 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball;
+var world;
+var radius = 40;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+
+	var ball_options = {
+		isStatic: false,
+		restitution: 0.8,
+		friction: 0,
+		density: 1.2
+	}
+	//Create the Bodies Here.
+	ball = Bodies.circle(260,100,radius/2,ball_options);
+	World.add(world,ball);
+
+	groundObj = new Ground(width/2,670,width,20);
+	leftside = new Ground(1100,600,20,120);
+	rightside = new Ground(1350,600,20,120)
+	
+	Engine.run(engine);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+
+  groundObj.display()
+  leftside.display()
+  rightside.display()
+
+  ellipse(ball.position.x,ball.position.y,radius,radius);
+
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
+    
+	}
+}
+
